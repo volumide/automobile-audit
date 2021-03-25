3b9192ec03e45c9716f62a4aab5f520fc8e711f6
Olumide fowowe <volumide42@gmail.com>
2021-03-25 03:13:28 +0100

    convert card result to json and log to the console

--- src/components/Review.vue
+++ src/components/Review.vue
@@ -1,7 +1,8 @@
 <template>
-	<div class="flex justify-center items-center h-screen  py-8 flex-col" v-if="complaint">
-		<p class=" p-3 font-bold text-blue-600 my-10 text-3xl">Automobile Audit Review Card</p>
-		<div class="w-80  py-10 px-8 rounded-xl bg-gray-100 shadow-lg ">
+	<div class="flex  items-center h-screen  py-6 flex-col" v-if="complaint">
+		<p class=" p-3 font-bold text-blue-600 my-6 text-3xl">Automobile Audit Review Card</p>
+		<!-- card design for outputing form result -->
+		<div class="w-96  py-10 px-10 rounded-xl bg-gray-100 shadow-lg ">
 			<div class="p-2">
 				<small class="font-bold text-blue-500">Name</small>
 				<p class="py-1 font-semibold text-lg">{{truncate(complaint.name)}}</p>
@@ -31,7 +32,7 @@
 				</ul>
 			</div>
 			<div class="p-2">
-				<button class="block w-full bg-blue-600 rounded p-4 font-semibold text-white">Submit</button>
+				<button class="block w-full bg-blue-600 rounded p-4 font-semibold text-white" @click="logJson">Submit</button>
 			</div>
 		</div>
 	</div>
@@ -40,15 +41,24 @@
 <script>
 import {onMounted, ref} from 'vue'
 export default {
+	// all functions use in this setup are in modern javascript es6
 	setup() {
+		// initialize complaint card value
 		const complaint = ref([])
-		const truncate = (word) =>(word&& word.length > 30 )? `${word.substring(0,29)}...` :word
+		// function to truncate name greater than 30 characeter 
+		const truncate = (word) =>(word && word.length > 30 )? `${word.substring(0,29)}...` : word
+
+		// function logs the displayed result to console in json format
+		const logJson = () => console.log(JSON.stringify(complaint.value))
+		// store audit report from local storage into complaint variable to bind to html
 		onMounted(() => {
 			complaint.value = JSON.parse(localStorage.getItem('accepted'))['_value']
 		})
 		return {
+			// return all ref and functions for dom manipulation
 			complaint,
-			truncate
+			truncate,
+			logJson
 		}
 	}
 }
